62893b10b9677321bf078d8aac88c1df3776c1bf
Camila Sena Araújo <camilasenaraujo@gmail.com>
2023-11-03 11:10:15 -0300

    Create repository

---
+++ main.js
@@ -0,0 +1,10 @@
+import './src/styles/settings/colors.css'
+import './src/styles/generic/reset.css'
+import './src/styles/elements/base.css'
+import CardGame from "./src/components/CardGame";
+import BoardGame from './src/objects/BoardGame';
+
+const $root = document.querySelector("#root") //boa prática: variavel referente a objeto da tela começa com $
+const $htmlBoardGame = BoardGame()
+
+$root.insertAdjacentHTML('beforeend', $htmlBoardGame);
---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+function CardGame() { 
+    return `
+    <article class = "card-game">
+        <img src="src/components/images/hollowKnight.png" alt="Logo do Hollow Knight">
+    </article>
+    `
+ }
+
+ export default CardGame;
