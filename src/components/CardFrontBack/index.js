c996704277bed7814cf1efb7a9c6766c7f9011fa
Camila Sena Araújo <camilasenaraujo@gmail.com>
2023-11-06 22:34:03 -0300

    testes

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,10 +1,15 @@
+import "./style.css"
 import CardGame from "../CardGame"
 
 function CardFrontBack() {
     return /*html*/ `
     <article class = "card-front-back">
+    <div class = "front">
     ${CardGame()}
+    </div>
+    <div class = "back">
     ${CardGame('Javascript')}
+    </div>
     </article>
     `
 }
--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -1,9 +1,9 @@
 import './style.css';
 
-function CardGame(icon = 'hkcapa.png',alt='capa do Holow Knight') { 
+function CardGame(icon = 'hkcapa',alt='capa do Holow Knight') { 
     return /*html*/`
     <article class = "card-game">
-        <img src="src/components/images/${icon}" alt="${alt}">
+        <img src="src/components/images/${icon}.png" alt="${alt}">
     </article>
     `
  }
