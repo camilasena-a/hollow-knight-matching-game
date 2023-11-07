20f23028ee4bc87f60bc3caec66017e8fd896568
Camila Sena Araújo <camilasenaraujo@gmail.com>
2023-11-07 10:16:15 -0300

    Add PlayerScore Component

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,12 +1,15 @@
 import "./style.css"
 import PlayerName from '../../components/PlayerName';
 import VsPlayer from "../../components/VsPlayer";
+import PlayerScore from "../../components/PlayerScore";
 
 function ScoreBoard() {
     return /*html*/ `
     <header class="score-board"> 
     ${PlayerName("Player 1")}
+    ${PlayerScore(1)}
     ${VsPlayer()}
+    ${PlayerScore(2)}
     ${PlayerName("Player 2")}
     </header>
     `
