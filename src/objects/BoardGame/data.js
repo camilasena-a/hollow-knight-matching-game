414a78a66a94523401ddf154f0a6a8d3a4a2d100
Camila Sena Araújo <camilasenaraujo@gmail.com>
2025-05-06 11:04:18 -0300

    adição de novos cards

--- src/objects/BoardGame/data.js
+++ src/objects/BoardGame/data.js
@@ -1,26 +1,50 @@
 const cards = [{
-    icon: "hollowKnight",
-    altIcon: "imagem da Hornert"
+    icon: "hornet",
+    altIcon: "imagem da Hornet"
 },
 {
-    icon: "hollowKnight",
-    altIcon: "imagem da Hornert"
+    icon: "hornet",
+    altIcon: "imagem da Hornet"
+},
+{
+    icon: "radiancia",
+    altIcon: "imagem da radiancia"
 },
 {
     icon: "radiancia",
     altIcon: "imagem da radiancia"
 },
 {
-    icon: "radiancia",
-    altIcon: "imagem da radiancia"
+    icon: "knight",
+    altIcon: "imagem do hollow"
+},
+{
+    icon: "knight",
+    altIcon: "imagem do hollow"
+},
+{
+    icon: "grimm",
+    altIcon: "imagem do grimm"
+},
+{
+    icon: "grimm",
+    altIcon: "imagem do grimm"
+},
+{
+    icon: "cornifer",
+    altIcon: "imagem do cornifer"
+},
+{
+    icon: "cornifer",
+    altIcon: "imagem do cornifer"
 },
 {
     icon: "reiPalido",
-    altIcon: "imagem da radiancia"
+    altIcon: "imagem do rei palido"
 },    
 {
     icon: "reiPalido",
-    altIcon: "imagem da radiancia"
+    altIcon: "imagem do rei palido"
 }];
 
 export default cards;
